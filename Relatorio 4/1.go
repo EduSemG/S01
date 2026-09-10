db49f022f1948b7e68e7738128b4a55a6efab582
EduSemG <dudslagatta@gmail.com>
2026-09-10 19:57:13 -0300

    Improve tracking code validation loop
    Refactor code to continuously prompt for tracking code until valid.

--- Relatorio 4/1.go
+++ Relatorio 4/1.go
@@ -5,7 +5,7 @@ func ValidarCodigoRastreio(codigo string) (bool, string) {
 	if len(codigo) == 10 {
     return true, "Codigo de rastreio registrado no sistema!"
 	} else {
-    return false, "Erro: O codigo de rastreio deve ter exatamente 10 caracteres."
+    return false, "Erro: O codigo de rastreio deve ter exatamente 10 caracteres.\n"
 }
 }
 
@@ -13,13 +13,17 @@ func main() {
 
 	fmt.Printf("Digite o código de rastreio: ")
 	var codigo string
-	fmt.Scanln(&codigo)
-
-	status, msg := ValidarCodigoRastreio(codigo)
-
-	if status == true {
-		fmt.Printf("\n%s",msg)
-	} else {
-		fmt.Printf("\n%s",msg)
+	
+	for true {
+		fmt.Scanln(&codigo)
+		status, msg := ValidarCodigoRastreio(codigo)
+		if status == true {
+			fmt.Printf("\n%s",msg)
+			break
+		} else {
+			fmt.Printf("\n%s",msg)
+			fmt.Printf("Digite o código de rastreio: ")
+		}
 	}
+
 }
